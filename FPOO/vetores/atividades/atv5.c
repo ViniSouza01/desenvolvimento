0fba05dac7d8ac2d631e0e34201cca4b1a0da582
ViniSouza07 <viniciusvieira130705@gmail.com>
2022-09-01 11:23:47 -0300

    ds

--- FPOO/vetores/atividades/atv5.c
+++ FPOO/vetores/atividades/atv5.c
@@ -9,15 +9,25 @@ strcpy(nomes[2];"elaine");*/
 int main(){
 	setlocale(LC_ALL,"");
     char nomes[5] [20];
-    strcpy(nomes[0];"Joaquim");
-    strcpy(nomes[1];"Ana");
-    strcpy(nomes[2];"Maria");
-    strcpy(nomes[3];"Paulo");
-    strcpy(nomes[4];"Fernando");
+    strcpy(nomes[0],"Joaquim");
+    strcpy(nomes[1],"Ana");
+    strcpy(nomes[2],"Maria");
+    strcpy(nomes[3],"Paulo");
+    strcpy(nomes[4],"Fernando");
     
 	int i;
     for(i = 0; i < 5; i++){
     	printf("%s\n",nomes[i]);
 	}
-	printf("%c")
+	//mostrar a letra 'm' de "Joaquim"
+	printf("%c\n",nomes[0][6]);
+	
+	//Mostrar o código ascii da letra "a" e "b"
+	printf("%d\n",'a');
+	printf("%d\n",'b');
+	int ascii;
+	char letra;
+	printf("Digite algum número:\n");
+	scanf("%c",&letra);
+	printf("%c\n",letra);
 }
