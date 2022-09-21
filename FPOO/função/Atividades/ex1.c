e8074ebbfc409613abf48a1cb65da9f2f452c8a9
ViniSouza01 <viniciusvieira130705@gmail.com>
2022-09-21 11:16:07 -0300

    das

---
+++ FPOO/função/Atividades/ex1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+	bool valores (int v1,int v2);
+int main (int aegc, char *argv[]){
+	int v1, v2, results;
+	scanf("%d",&v1);
+	scanf("%d",&v2);
+	printf("%d",);
+	return 0;
+}
+bool valores(int v1,int v2){
+	if(v1 == v2){
+		return true;
+	}else {
+		return false;
+	}
+	return valores;
+}
--- FPOO/vetores/Desafios/Desafio1.c
+++
@@ -1,2 +0,0 @@
-/*Desafio 1- falar quais as posições estão repetidas*/
-
