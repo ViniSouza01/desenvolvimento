66e86a027677ce14771ccf5da6750a6f1a679d1f
ViniSouza01 <viniciusvieira130705@gmail.com>
2022-09-23 09:12:47 -0300

    atividade retângulo

--- FPOO/Funções/Retângulo/atv.c
+++ FPOO/Funções/Retângulo/atv.c
@@ -1,13 +1,28 @@
 #include<stdio.h>
 #include<locale.h>
-
+int retangulo();
 int main(){
 	setlocale(LC_ALL,"");
-	int largura, altura;
-	char string;
-	printf("Digite a largura do retângulo:\n");
+	int altura, largura, ret;
+	char string[5];
+	printf("Digite uma largura para o retangulo:\n");
 	scanf("%d",&largura);
-	printf("Digite a largura do retângulo:\n");
+	printf("DIgite uma altura para o retangulo:\n");
 	scanf("%d",&altura);
+	printf("Digite o caracter que você deseja fazer o retângulo:\n");
+	scanf("%s",&string);
+	ret = retangulo(altura, largura, string); //mostra o desenho do retângulo
 }
 
+// Desenho do retângulo com repetidores
+int retangulo(altura, largura, string){
+	int i = 0,j = 0;
+	while(i < altura){
+		for(j = 0; j < largura; j++){
+			printf("%s",string);
+		}
+		printf("\n");
+		i++;
+	}
+	return retangulo;
+}
