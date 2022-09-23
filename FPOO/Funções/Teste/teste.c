b97d992bb2dfbd67795350dc4dc9372ead8a1fff
ViniSouza01 <viniciusvieira130705@gmail.com>
2022-09-23 11:17:06 -0300

    ds

--- FPOO/Funções/Teste/teste.c
+++ FPOO/Funções/Teste/teste.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
-#include<locale.h>
+int retangulo(int altura, int largura);
 int main(){
-	setlocale(LC_ALL,"");
-	char string[10];
-	printf("Digite um caracter:\n");
-	gets(string);  //lê qualquer tipo de caractere
-	printf("Você digitou o caracter %s",string); //mostra na tela o caractere que você digitou
-	return 0;
+	int x,y;
+	char c;
+	printf("Digite o comprimento:\n");
+	scanf("%d",&x);
+	printf("Digite a altura:\n");
+	scanf("%d",&y);
+	ret = retangulo()
+}
+int retangulo(int altura, int largura){
+	
 }
