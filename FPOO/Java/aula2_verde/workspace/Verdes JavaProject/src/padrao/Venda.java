db2fdd4adec1744dba89eeaff401b77a45ba9ea4
ViniSouza01 <viniciusvieira130705@gmail.com>
2022-10-07 09:53:48 -0300

    ds

---
+++ FPOO/Java/aula2_verde/workspace/Verdes JavaProject/src/padrao/Venda.java
@@ -0,0 +1,13 @@
+package padrao;
+
+public class Venda {
+	// attributes
+	String produto;
+	float preco;
+	int quantidade;
+	
+	// it's a function, but as it's inside the class, it is named as method
+	float subtotal() {
+		return preco * quantidade;
+	}
+}
