d95783e13b0ea9a095319262c5804c9c0a504512
ViniSouza01 <viniciusvieira130705@gmail.com>
2022-10-17 11:23:58 -0300

    Correção aula 13 FPOO

--- FPOO/Java/aula2_verde/workspace/Clinica/src/Pack/Main.java
+++ FPOO/Java/aula2_verde/workspace/Clinica/src/Pack/Main.java
@@ -6,8 +6,11 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		Locale.setDefault(new Locale("pt","BR"));
 		Scanner scan = new Scanner(System.in);
 		Paciente pacientes[] = new Paciente[10]; //10 objects of the class "Paciente"
+		float totImc = 0, totPeso = 0, totAltura = 0;
+		//input
 		System.out.println("Digite 10 nomes, peso e altura"); //Incoming message
 		for(int i = 0; i < 10; i++) { //loop to feel the array
 			pacientes[i] = new Paciente(); //create unique object
@@ -15,8 +18,19 @@ public class Main {
 			pacientes[i].peso = scan.nextFloat();
 			pacientes[i].altura = scan.nextFloat();
 		}
+		//output
 		for(int i = 0; i < 10; i++) {
 			System.out.println(pacientes[i].toString());
+			totImc += pacientes[i].imc();
+			totPeso += pacientes[i].peso;
+			totAltura += pacientes[i].altura;
 		}
+		float mediaImc = totImc / 10f;
+		float mediaPeso = totPeso / 10f;
+		float mediaAltura = totAltura / 10f;
+		System.out.println("Estatísticas:");
+		System.out.printf("Média IMC = %.2f\n",mediaImc);
+		System.out.printf("Média Peso = %.2f\n",mediaPeso);
+		System.out.printf("Média Altura = %.2f\n",mediaAltura);
 	}
 }
